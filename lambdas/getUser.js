fffcdfa9c6f3b785ac4b8acd67d369e96a1573a0
Gabriel <rochafrgabriel@gmail.com>
2022-03-11 17:08:50 -0300

    Final code

--- lambdas/getUser.js
+++ lambdas/getUser.js
@@ -1,17 +1,27 @@
-const Responses = require('./API_Responses');
-// import { addDays, format } from "date-fns"
+const Responses = require('./API_Responses')
 
-const { addDays } = require('date-fns')
-const { format } = require('date-fns')
+const { addDays, format } = require('date-fns')
 
 exports.handler = async event => {
     console.log('event', event);
 
+    // alreadyPalin checa se a data entregue pela request já é um palindromo.
+    //Se viu necessidade da criação dessa função quando a data enviada na request já era uma data palindromo.
+    const alreadyPalin = (ano, mes, dia) =>{
+        const check = new Date(ano, mes, dia, 0, 0, 0, 0)
+        const checkString = format(check, 'dd/MM/yyyy')
+    
+        if(checkString != inverterData(check)){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+
     const inverterData = (data) =>{
         const dataEmString = format(data, 'dd/MM/yyyy')
-        
         const dataSemBarras = dataEmString.split('/').join('')
-    
         const digitoFinalDia = 1
         const digitoFinalMes = 3
     
@@ -24,69 +34,46 @@ exports.handler = async event => {
         return dataReversa
     }
     
-    const quandoProxDataPalindroma = (ano, mes, dia) => {
-        const hoje = new Date(ano, mes, dia, 1, 1, 1, 1)
-    
+    const quandoProxDataPalindromo = (ano, mes, dia) => {
+        const hoje = new Date(ano, mes, dia, 0, 0, 0, 0)
         let proxDataPalin
-    
         let possivelProxDataPalin = hoje
 
         for(possivelProxDataPalin; format(possivelProxDataPalin, 'dd/MM/yyyy') != inverterData(possivelProxDataPalin); possivelProxDataPalin = addDays(possivelProxDataPalin, 1)){
             proxDataPalin = (addDays(possivelProxDataPalin, 1))
         }
-    
+
         return { dia: format(proxDataPalin, 'dd'), mes:format(proxDataPalin, 'MM'), ano: format(proxDataPalin, 'yyyy')}
     }
     
-    const alreadyPalin = (ano, mes, dia) =>{
-        const check = new Date(ano, mes, dia, 0, 0,0,0)
-        const checkString = format(check, 'dd/MM/yyyy')
-    
-        if(checkString != inverterData(check)){
-            return true
-        }
-        else{
-            return false
-        }
-    }
-
-
     let data = event.pathParameters.ID
-
-
     const requestData = data.toString().split('')
     
     const ano = requestData[0]+requestData[1]+requestData[2]+requestData[3];
-    
     const mes = requestData[4]+requestData[5] - 1
     const dia = requestData[6]+requestData[7]
     
     if(alreadyPalin(ano,mes,dia) == true){
-        return Responses._200(quandoProxDataPalindroma(ano , mes, dia))
+        return Responses._200(quandoProxDataPalindromo(ano , mes, dia))
     }
-    else{
+    if((alreadyPalin(ano,mes,dia) != true)){
+        // incremento no dia, para a data deixar de ser um palíndromo 
         const novoDia = parseInt(dia) + 1
-        return Responses._200(quandoProxDataPalindroma(ano , mes, novoDia))
+        return Responses._200(quandoProxDataPalindromo(ano , mes, novoDia))
     }
-    // const proxDataPalin = quandoProxDataPalindroma(ano , mes, dia)
     
-    // console.log('a proxima data palindroma eh: ', proxDataPalin)
-    
-    // (quandoProxDataPalindroma(ano , mes, dia))
-
     if (!event.pathParameters || !event.pathParameters.ID) {
-        // failed without an ID
+        // falha - sem ID
         return Responses._400({ message: 'missing the parameters from the path' });
     }
 
-    // if (requestData.length == 8) {
-    //     // return the data
-    //     console.log(quandoProxDataPalindroma(ano , mes, dia))
-    //     return Responses._200((quandoProxDataPalindroma(ano , mes, dia)));  
-    // }
-
     if (requestData.length < 8) {
-        // return message error
+        // retorna mensagem de erro
+        return Responses._400({message: 'api request not in correct format'});
+    }
+
+    if (requestData.length > 8) {
+        // retorna mensagem de erro
         return Responses._400({message: 'api request not in correct format'});
     }
 
